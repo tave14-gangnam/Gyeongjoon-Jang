cdfdac001a1c06a0f677d74dd251f61177f7d2fe
GyeongJoon <jjangjoon328@naver.com>
2024-09-21 17:29:52 +0900

    [base]Member/Entity 생성

---
+++ src/main/java/com/tave/gangnam/member/domain/Member.java
@@ -0,0 +1,33 @@
+package com.tave.gangnam.member.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor // Constructor
+@Builder
+@NoArgsConstructor // 기본 생성자
+public class Member {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // AllArgsConstructor
+//    public Member(Long id, String name) {
+//        this.id = id;
+//        this.name = name;
+//    }
+
+    // 생성자: TODO 생성자를 만드는 이유
+//    public Member() {}
+
+    //
+}
