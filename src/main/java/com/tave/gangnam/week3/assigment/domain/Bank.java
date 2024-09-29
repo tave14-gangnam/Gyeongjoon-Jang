2668501bff0f6500250ccc417df5733add36cf4e
GyeongJoon <jjangjoon328@naver.com>
2024-09-29 15:59:47 +0900

    [feat] Customer,Bank/Entity 생성

--- src/main/java/com/tave/gangnam/week3/assigment/domain/Bank.java
+++ src/main/java/com/tave/gangnam/week3/assigment/domain/Bank.java
@@ -1,5 +1,28 @@
 package com.tave.gangnam.week3.assigment.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter // 모든 필드에 대해 getter 메소드를 자동으로 생성
+@NoArgsConstructor // 매개변수가 없는 기본 생성자를 자동으로 생성 (new Bank())
+@AllArgsConstructor // 모든 필드를 매개변수로 받는 전체 생성자를 자동으로 생성
+@Builder // 객체를 생성할 때 필드 값을 순차적으로 설정
 public class Bank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bankId;
+
+    private Long loan;
+
+    private Long installmentSavings;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customerId")
+    private Customer customer;
 
 }
--- src/main/java/com/tave/gangnam/week3/assigment/domain/Customer.java
+++ src/main/java/com/tave/gangnam/week3/assigment/domain/Customer.java
@@ -1,4 +1,29 @@
 package com.tave.gangnam.week3.assigment.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    private int age;
+
+    private int password;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Bank> banks = new ArrayList<>();
 }
