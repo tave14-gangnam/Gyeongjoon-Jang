c6fbeda7c785b95b15d6262c18b869248a5b4aeb
GyeongJoon <jjangjoon328@naver.com>
2024-09-29 16:42:53 +0900

    [feat] Customer,Bank/Repository 기능 추가

--- src/main/java/com/tave/gangnam/week3/assigment/repository/BankRepository.java
+++ src/main/java/com/tave/gangnam/week3/assigment/repository/BankRepository.java
@@ -2,6 +2,8 @@ package com.tave.gangnam.week3.assigment.repository;
 
 import com.tave.gangnam.week3.assigment.domain.Bank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BankRepository extends JpaRepository<Bank, Long> {
 }
--- src/main/java/com/tave/gangnam/week3/assigment/repository/CustomerRepository.java
+++ src/main/java/com/tave/gangnam/week3/assigment/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.tave.gangnam.week3.assigment.repository;
 
 import com.tave.gangnam.week3.assigment.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
